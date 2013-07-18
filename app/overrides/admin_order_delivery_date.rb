c750e42d56764eb45546c5081afde0c964f2d656
vinni101 <vinni101@gmail.com>
2013-07-17 20:07:53 -0700

    Create admin_order_delivery_date.rb

---
+++ app/overrides/admin_order_delivery_date.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/admin/orders/_form",
+                      :name => "add_delivery_date_to_admin_order_information",
+                      :insert_top => ".filter-actions",
+                      :partial => "spree/admin/orders/delivery_date",
+                      :disabled => false)
