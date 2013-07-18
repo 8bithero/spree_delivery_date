f8bd067ce0d10bf8a0302af2b50dd716f5fa67f0
vinni101 <vinni101@gmail.com>
2013-07-18 08:05:18 -0700

    Update admin_order_delivery_date.rb

--- app/overrides/admin_order_delivery_date.rb
+++ app/overrides/admin_order_delivery_date.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
                       :name => "add_delivery_date_to_admin_order_information",
-                      :insert_before => "#order-form-wrapper",
+                      :insert_top => "[data-hook='toolbar']>ul",
                       :partial => "spree/admin/orders/delivery_date",
                       :disabled => false)
