0d22f9006c91eb1b134979ab9a630954bccf8089
vinni101 <vinni101@gmail.com>
2013-07-17 20:28:49 -0700

    Update admin_order_delivery_date.rb

--- app/overrides/admin_order_delivery_date.rb
+++ app/overrides/admin_order_delivery_date.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/orders/_form",
                       :name => "add_delivery_date_to_admin_order_information",
-                      :insert_top => "[data-hook='admin_order_form_buttons']",
+                      :insert_top => "[data-hook='order_details_total']",
                       :partial => "spree/admin/orders/delivery_date",
                       :disabled => false)
