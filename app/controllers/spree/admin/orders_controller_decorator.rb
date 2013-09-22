b626f51aa597e5422740532569631516518616c3
vinni101 <vinni101@gmail.com>
2013-09-21 22:13:43 -0400

    Fixed date

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -9,11 +9,11 @@ Spree::Admin::OrdersController.class_eval do
     
     #@order=Spree::Order.find_by_number(params[:id])
     order=Spree::Order.find_by_number(params[:id])
-    delivery_date2 = Time.local(params[:order]["delivery_date(1i)"].to_i, 
+    delivery_date2 = Time.new(params[:order]["delivery_date(1i)"].to_i, 
                             params[:order]["delivery_date(2i)"].to_i,
                             params[:order]["delivery_date(3i)"].to_i,
                             params[:order]["delivery_date(4i)"].to_i,
-                            params[:order]["delivery_date(5i)"].to_i)
+                            params[:order]["delivery_date(5i)"].to_i,00, "-04:00")
     #render :text => params[:order]["delivery_date(1i)"]
     #@order.validate
     order.delivery_date = delivery_date2
